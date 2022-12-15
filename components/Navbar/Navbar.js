77d3071fd8df6fa6d5ac05e4a2f90b4c848e6e95
vanamgangireddy <76478743+vanam93@users.noreply.github.com>
2022-12-15 17:34:10 +0530

    changes

---
+++ components/Navbar/Navbar.js
@@ -0,0 +1,59 @@
+import React from "react";
+import './Navbar.css';
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid">
+          <NavLink className="navbar-brand ms-3" to="/">
+            Title
+          </NavLink>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item me-3">
+                <NavLink className="nav-link" to="/influencers">
+                  Influencers
+                </NavLink>
+              </li>
+              <li className="nav-item me-3">
+                <NavLink className="nav-link" to="/about">
+                  My Profile
+                </NavLink>
+              </li>
+              <li className="nav-item me-3">
+                <NavLink className="nav-link" to="/register">
+                  Register
+                </NavLink>
+              </li>
+              <li className="nav-item me-3">
+                <NavLink className="nav-link" to="/login">
+                  Login
+                </NavLink>
+              </li>
+              <li className="nav-item me-3">
+                <NavLink className="nav-link" to="/logout">
+                  Logout
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
